e52e1686b97703ada3071e72ba31ed4aacb408cd
3ndymi0n <endymi0n@systemdecay.info>
2025-08-07 21:38:25 +1000

    Beginning adding mongoDB functionality and DB Model

--- backend/index.js
+++ backend/index.js
@@ -1,46 +1,24 @@
+require('dotenv').config()
+console.log(process.env.MONGODB_URI)
 const express = require('express')
 const cors = require('cors')
+const Note = require('./models/notes')
+
 const app = express()
+
 
 app.use(cors())
 app.use(express.json())
 
-let notes = [
-  {
-    id: "1",
-    content: "HTML is easy",
-    important: true
-  },
-  {
-    id: "2",
-    content: "Browser can execute only JavaScript",
-    important: false
-  },
-  {
-    id: "3",
-    content: "GET and POST are the most important methods of HTTP protocol",
-    important: true
-  },
-  {
-    id: "4",
-    content: "GET and POST are the most important methods of HTTP protocol",
-    important: true
-  }
-]
-
-const generateId = () => {
-    const maxId = notes.length > 0
-        ? Math.max(...notes.map(n => Number(n.id)))
-        : 0
-    return String(maxId + 1)
-}
 
 app.get('/', (request, response) => {
     response.send('<h1>Hello World!</h1>')
 })
 
 app.get('/api/notes', (request, response) => {
-    response.json(notes)
+    Note.find({}).then(notes => {
+        response.json(notes)
+    })
 })
 
 app.get('/api/notes/:id', (request, response) => {
@@ -81,7 +59,7 @@ app.post('/api/notes', (request, response) => {
     response.json(note)
 })
 
-const PORT=process.env.PORT || 3001
+const PORT=process.env.PORT
 app.listen(PORT, () => {
     console.log(`Server running on ${PORT}.`)
 })
