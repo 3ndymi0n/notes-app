12f61247d21fe8ba145c3e81db00ed142f557156
3ndymi0n <endymi0n@systemdecay.info>
2025-08-07 22:19:43 +1000

    Updated index.js functions to work with MongoDB

--- backend/index.js
+++ backend/index.js
@@ -1,65 +1,57 @@
-require('dotenv').config()
-console.log(process.env.MONGODB_URI)
-const express = require('express')
-const cors = require('cors')
-const Note = require('./models/notes')
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const Note = require("./models/notes");
 
-const app = express()
+const app = express();
 
+app.use(cors());
+app.use(express.json());
 
-app.use(cors())
-app.use(express.json())
+app.get("/", (request, response) => {
+  response.send("<h1>Hello World!</h1>");
+});
 
+app.get("/api/notes", (request, response) => {
+  Note.find({}).then((notes) => {
+    response.json(notes);
+  });
+});
 
-app.get('/', (request, response) => {
-    response.send('<h1>Hello World!</h1>')
-})
-
-app.get('/api/notes', (request, response) => {
-    Note.find({}).then(notes => {
-        response.json(notes)
-    })
-})
-
-app.get('/api/notes/:id', (request, response) => {
-    const id = request.params.id
-    const note = notes.find(note => note.id === id)
-
-    if (note) {
-        response.json(note)
-    } else {
-        response.status(404).end()
-    }
-})
-
-app.delete('/api/notes/:id', (request, response) => {
-    const id = request.params.id
-    notes = notes.filter(note => note.id !== id)
-
-    response.status(204).end()
-})
-
-app.post('/api/notes', (request, response) => {
-    const body = request.body
-
-    if (!body.content) {
-        return response.status(404).json({
-            error: 'content missing'
-        })
-    }
-
-    const note = {
-        content: body.content,
-        important: body.important || false,
-        id: generateId()
-    }
-
-    notes = notes.concat(note)
-    console.log(notes)
+app.get("/api/notes/:id", (request, response) => {
+  Note.findById(request.params.id).then(note => {
     response.json(note)
-})
+  })
+});
 
-const PORT=process.env.PORT
+app.delete("/api/notes/:id", (request, response) => {
+  const id = request.params.id;
+  notes = notes.filter((note) => note.id !== id);
+
+  response.status(204).end();
+});
+
+app.post("/api/notes", (request, response) => {
+  const body = request.body;
+
+  if (!body.content) {
+    return response.status(404).json({
+      error: "content missing",
+    });
+  }
+
+  const note = new Note({
+    content: body.content,
+    important: body.important || false,
+  })
+
+  note.save().then(savedNote => {
+    response.json(savedNote)
+  })
+
+});
+
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
-    console.log(`Server running on ${PORT}.`)
-})
+  console.log(`Server running on ${PORT}.`);
+});
